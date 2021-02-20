e584fd60f28a63fd1040932af997151018df914a
Christopher Hui <chui@coursera.org>
2021-02-19 20:13:01 -0800

    functional components and made button more clearer

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -2,31 +2,23 @@ import React from 'react';
 import { auth } from "../firebase";
 import Button from '@material-ui/core/Button'
 
-class Landing extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.logout = this.logout.bind(this);
-  }
-
-  logout() {
+const Landing = () => {
+  const logout = () => {
     auth.signOut();
   }
 
-  render() {
-    return (
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '90vh'
-        }}
-      >
-        <Button onClick={this.logout}>log out</Button>
-      </div>
-    );
-  }
-};
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '90vh'
+      }}
+    >
+      <Button variant="contained" color="primary" onClick={logout}>Log Out</Button>
+    </div>
+  );
+}
 
 export default Landing;
