9d715ab441d93557b81212d9a3d4f02947442620
Pectacius <shenpaiwilliam@gmail.com>
2021-02-20 15:49:34 -0800

    complete flask api

--- RESTApp/app.py
+++ RESTApp/app.py
@@ -1,3 +1,5 @@
+import hashlib
+
 from flask import Flask, jsonify, request
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -65,8 +67,8 @@ class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(256), index=True)
     owners = db.relationship('User', secondary=ownership, back_populates='projects')
-    todos = db.relationship('TodoItem', back_populates='project')
-    elements = db.relationship('ProjectElement', back_populates='project')
+    todos = db.relationship('TodoItem', cascade="all,delete", back_populates='project')
+    elements = db.relationship('ProjectElement', cascade="all,delete", back_populates='project')
 
     def __repr__(self) -> str:
         '''
@@ -119,7 +121,7 @@ class TodoItem(db.Model):
             'title': self.title,
             'description': self.description,
             'completed': self.completed,
-            'due_date': self.due_date.isoformat(),
+            'due_date': self.due_date,
             'project_id': self.project_id
         }
         return toDict
@@ -179,13 +181,24 @@ def error_response(status_code, message=None):
 #   that the live app is run i.e running locally would be localhost and on a vps
 #   would be the url that the vps provides.
 
-@app.route('/user/<int:user_id>', methods=['GET'])
-def get_user(user_id: int):
+@app.route('/user/<string:user_id>', methods=['GET'])
+def get_user(user_id: str):
     '''
     Send GET request to "domainname.com/user/user_id" where user_id is the
     number of the user id to get infomation on.
     '''
-    user = User.query.filter_by(id = user_id)[0];
+    n = int(hashlib.sha256(user_id.encode('utf-8')).hexdigest(), 16) % 10**8
+    user = User.query.filter_by(id = n).first();
+    return jsonify(user.toJSON())
+
+
+@app.route('/user/username/<string:username>', methods=['GET'])
+def get_user_by_username(username: str):
+    '''
+    Send GET request to "domainname.com/user/username/<username>" where <username> is the
+    username of the user to get infomation on.
+    '''
+    user = User.query.filter_by(username=username).first()
     return jsonify(user.toJSON())
 
 
@@ -198,19 +211,18 @@ def create_user():
     '''
     data = request.get_json()
     user = User.query.filter_by(username=data['username']).first()
-    print(user)
     if user:
         return error_response(400, 'bad request')
     else:
         def callback(session):
             data = request.get_json()
-            newUser = User(username=data['username'], password=data['password'])
+            n = int(hashlib.sha256(data['id'].encode('utf-8')).hexdigest(), 16) % 10**8
+            newUser = User(id=n, username=data['username'], password=data['password'])
             session.add(newUser)
             return jsonify(newUser.toJSON())
         return run_transaction(sessionmaker, callback)
         
 
-"""
 @app.route('/login', methods=['POST'])
 def validate_user():
     '''
@@ -221,22 +233,31 @@ def validate_user():
     Validation is conducted by checking if the username password pair is in the
     database. NOTE passwords are stored unhashed and unsalted. USE WITH CAUTION
     '''
-    pass
+    data = request.get_json()
+    user = User.query.filter_by(username=data['username']).first()
+    if user:
+        return jsonify(user.toJSON())
+    else:
+        return error_response(400, 'bad request')
 
 
 @app.route('/project/<int:project_id>', methods=['GET'])
-def get_project():
+def get_project(project_id: int):
     '''
     Send GET request to "domainname.com/project/project_id" where project_id is
     the number of the id of the project that would like to be retrieved. Will
     return the project that matches the given project_id or None if there are
     no matches
     '''
-    pass
+    project = Project.query.filter_by(id=project_id).first()
+    if project:
+        return jsonify(project.toJSON())
+    else:
+        return error_response(400, 'bad request')
 
-
+"""
 @app.route('/similar/<int:project_id>', methods=['GET'])
-def get_similar_projects():
+def get_similar_projects(project_id: int):
     '''
     Send GET request to "domainname.com/similar/project_id" where project_id
     is the project that would like to be used as the comparision to find other
@@ -244,19 +265,34 @@ def get_similar_projects():
     matches.
     '''
     pass
+"""
 
-
-@app.route('/user/project/<int:user_id>', method=['GET'])
-def get_user_projects():
+@app.route('/user/project/<string:user_id>', methods=['GET'])
+def get_user_projects(user_id: str):
     '''
     Send GET request to "domainname.com/user/similar/user_id" where the user_id
     is the user that is the owner(s) of the projects that would like to be searched.
     Will return all projects that have the specified user as the owner or none if
     there are no matches.
     '''
-    pass
+    n = int(hashlib.sha256(user_id.encode('utf-8')).hexdigest(), 16) % 10**8
+    project = Project.query.filter(Project.owners.any(id=n));
+    if project:
+        return jsonify([p.toJSON() for p in project])
+    else:
+        return error_response(400, 'bad request')
 
 
+@app.route('/user/project/<string:user_id>/<string:project_name>', methods=['GET'])
+def get_project_by_name(user_id:str, project_name:str):
+    n = int(hashlib.sha256(user_id.encode('utf-8')).hexdigest(), 16) % 10**8
+    project = Project.query.filter(Project.owners.any(id=n)).filter_by(name=project_name).first()
+    if project:
+        return jsonify(project.toJSON())
+    else:
+        return error_response(400, 'bad request')
+
+"""
 @app.route('/user/similar/<int:user_id>', methods=['GET'])
 def get_user_similar_projects():
     '''
@@ -266,26 +302,90 @@ def get_user_similar_projects():
     has. Will return all projects similar or None if there are no matches.
     '''
     pass
+"""
 
-
-@app.route('/create', methods=['POST'])
-def create_project():
+@app.route('/create/<string:user_id>', methods=['POST'])
+def create_project(user_id: str):
     '''
     Send POST request to "domainname.com/create" where the request body contains
     the data for a new project.
     '''
-    pass
+    n = int(hashlib.sha256(user_id.encode('utf-8')).hexdigest(), 16) % 10**8
+    # check if user exists in db because a project must have an owner
+    user = User.query.filter_by(id=n).first()
+    if user:
+        data = request.get_json()
+        project = Project.query.filter(Project.owners.any(id=user_id)).filter_by(name=data['name']).first()
+        if project:
+            return error_response(400, 'project already exists')
+        else:
+            def callback(session):   
+                newProject = Project(name=data['name'])
+                newProject.owners.append(user)
+                 
+        
+    
+                todoItems = data['todo']
+                elementItems = data['element']
+
+                for item in todoItems:
+                    newTodo = TodoItem(title=item['title'], description=item['description'], completed=False)
+                    newProject.todos.append(newTodo)
+
+                for item in elementItems:
+                    newElement = ProjectElement(data=item['data'], type=item['type'])
+                    newProject.elements.append(newElement)
+
+                project = session.merge(newProject)
+                session.add(project) 
+                return jsonify(newProject.toJSON())
+            return run_transaction(sessionmaker, callback)
+    else:
+        return error_response(400, 'user does not exist')
 
 
 @app.route('/update/<int:project_id>', methods=['POST'])
-def update_project():
+def update_project(project_id: int):
     '''
     Send POST request to "domainname.com/update/project_id" where project_id is
     the integer representing the id of the project to be updated. The request body
     contains the data for the new version of the project.
     '''
-    pass
-"""
+    project = Project.query.filter_by(id=project_id).first()
+    if project:
+        data = request.get_json()
+        if data['todo']:
+            for t in project.todos:
+                project.todos.remove(t)
+            for item in data['todo']:
+                newTodo = TodoItem(title=item['title'], description=item['description'], completed=False)
+                project.todos.append(newTodo)
+        if data['element']:
+            for e in project.elements:
+                project.elements.remove(e)
+            for item in data['element']:
+                newElement = ProjectElement(data=item['data'], type=item['type'])
+                project.elements.append(newElement)
+        db.session.commit()
+        return jsonify(project.toJSON())
+    else:
+        return error_response(400, 'project does not exist')
+
+
+@app.route('/adduser/<int:project_id>', methods=['POST'])
+def add_user_to_project(project_id: int):
+    project = Project.query.filter_by(id=project_id).first()
+    data = request.get_json()
+    user_id = data['id']
+    n = int(hashlib.sha256(user_id.encode('utf-8')).hexdigest(), 16) % 10**8
+    user = User.query.filter_by(id=n).first()
+    if (project and user):
+        project.owners.append(user)
+        db.session.commit()
+        return jsonify(project.toJSON())
+    else:
+        return error_response(400, 'project does not exist')
+
 
 if __name__ == '__main__':
     app.run()
