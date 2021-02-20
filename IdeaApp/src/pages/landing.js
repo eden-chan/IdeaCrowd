fada572e78e06d71f8bede907ea06912555756da
Christopher Hui <chui@coursera.org>
2021-02-20 00:05:46 -0800

    sidebar change

--- IdeaApp/src/pages/landing.js
+++ IdeaApp/src/pages/landing.js
@@ -1,24 +1,31 @@
-import React from 'react';
+import React from "react";
 import { auth } from "../firebase";
-import Button from '@material-ui/core/Button'
+import Button from "@material-ui/core/Button";
+import Layout from "../components/Layout";
+import "react-pro-sidebar/dist/css/styles.css";
 
 const Landing = () => {
   const logout = () => {
     auth.signOut();
-  }
+  };
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '90vh'
-      }}
-    >
-      <Button variant="contained" color="primary" onClick={logout}>Log Out</Button>
+    <div>
+      <Layout />
+      {/* <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "90vh",
+        }}
+      >
+        <Button variant="contained" color="primary" onClick={logout}>
+          Log Out
+        </Button>
+      </div> */}
     </div>
   );
-}
+};
 
 export default Landing;
